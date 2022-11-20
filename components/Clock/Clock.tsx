177c1ac01ec4740930de9bb5db0a6d7af44b4b67
fecapark <fecapark@gmail.com>
2022-11-20 04:48:28 +0000

    Feature: Prevent Clock handler degree over 100%

--- components/Clock/Clock.tsx
+++ components/Clock/Clock.tsx
@@ -18,40 +18,75 @@ export default function Clock() {
     if (!moveAreaRef.current) return;
     if (!handlerRef.current) return;
 
+    const getRelPos = (offsetPos: Vector2) => {
+      return offsetPos.sub(centerPos.sub(moveAreaPos));
+    };
+
+    const getRotationDegree = (offsetPos: Vector2) => {
+      const relPos = getRelPos(offsetPos).normalize();
+      const isRightSide = relPos.x >= 0;
+      let degree = Math.acos(relPos.dot(new Vector2(0, -1))) * (180 / Math.PI);
+      degree = isRightSide ? degree : 360 - degree;
+
+      return degree;
+    };
+
     const pointerDownHandler = (e: PointerEvent) => {
       canSet = true;
+      stop = false;
 
       const offsetPos = new Vector2(e.offsetX, e.offsetY);
-      const relPos = offsetPos.sub(centerPos.sub(moveAreaPos)).normalize();
-      const isRightSide = relPos.x >= 0;
-      const cos = Math.acos(relPos.dot(new Vector2(0, -1))) * (180 / Math.PI);
+      const degree = getRotationDegree(offsetPos);
 
       requestAnimationFrame(() => {
-        handlerRef.current!.style.transform = `translate3d(-50%, -50%, 0) rotate3d(0, 0, 1, ${
-          isRightSide ? 360 + cos : -cos
-        }deg)`;
+        handlerRef.current!.style.transform = `
+          translate3d(-50%, -50%, 0) 
+          rotate3d(0, 0, 1, ${degree}deg)
+        `;
+        moveAreaRef.current!.style.background = `
+          conic-gradient(#00000000 ${degree}deg, #FA3141dd ${degree}deg)
+        `;
       });
     };
 
     const pointerMoveHandler = (e: PointerEvent) => {
       if (!canSet) return;
+
       const offsetPos = new Vector2(e.offsetX, e.offsetY);
-      const relPos = offsetPos.sub(centerPos.sub(moveAreaPos)).normalize();
-      const isRightSide = relPos.x >= 0;
-      const cos = Math.acos(relPos.dot(new Vector2(0, -1))) * (180 / Math.PI);
+      const relPos = getRelPos(offsetPos);
+      let degree = stop ? 0 : getRotationDegree(offsetPos);
+
+      if (
+        relPos.y <= 0 &&
+        relPos.x < 0 &&
+        prevRelPos !== null &&
+        prevRelPos.x >= 0
+      ) {
+        stop = true;
+        degree = 0;
+      } else if (relPos.y <= 0 && relPos.x >= 0) {
+        stop = false;
+      }
+      prevRelPos = relPos.copy();
 
       requestAnimationFrame(() => {
-        handlerRef.current!.style.transform = `translate3d(-50%, -50%, 0) rotate3d(0, 0, 1, ${
-          isRightSide ? 360 + cos : -cos
-        }deg)`;
+        handlerRef.current!.style.transform = `
+          translate3d(-50%, -50%, 0) 
+          rotate3d(0, 0, 1, ${degree}deg)
+        `;
+        moveAreaRef.current!.style.background = `
+          conic-gradient(#00000000 ${degree}deg, #FA3141dd ${degree}deg)
+        `;
       });
     };
 
     const pointerEndHandler = () => {
       canSet = false;
+      stop = false;
     };
 
     let canSet = false;
+    let stop = false;
     const {
       width,
       height,
@@ -62,6 +97,7 @@ export default function Clock() {
 
     const moveAreaPos = new Vector2(ax, ay);
     const centerPos = new Vector2(hx + width / 2, hy + height / 2);
+    let prevRelPos: Vector2 | null = null;
 
     moveAreaRef.current.addEventListener("pointerdown", pointerDownHandler);
     moveAreaRef.current.addEventListener("pointermove", pointerMoveHandler);
@@ -86,13 +122,13 @@ export default function Clock() {
     <Container>
       <MainClock>
         <ClockCenter>
+          {range(60).map((i) => (
+            <Graduation rotate={i * 6} accent={i % 5 == 0} key={i}></Graduation>
+          ))}
           <ClockBackground ref={moveAreaRef} />
           <ClockHandler ref={handlerRef}>
             <div className="pointer"></div>
           </ClockHandler>
-          {range(60).map((i) => (
-            <Graduation rotate={i * 6} accent={i % 5 == 0} key={i}></Graduation>
-          ))}
         </ClockCenter>
       </MainClock>
     </Container>
