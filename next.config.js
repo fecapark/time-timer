fce829cf4e64858908ecae1a9a9d03d7bb4cfe60
fecapark <fecapark@gmail.com>
2022-11-29 02:27:45 +0000

    Chore: Change next.config.js of config.basePath - separate dev/production base path

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const isDevServer = process.env.NODE_ENV === "development";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: "/absproxy/3000",
+  basePath: isDevServer ? "/absproxy/3000" : "",
 };
 
 module.exports = nextConfig;
