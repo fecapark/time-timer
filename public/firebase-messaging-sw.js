07749ce6c9d928815d19de199fbb73bb1a1c224a
fecapark <fecapark@gmail.com>
2022-12-01 11:55:51 +0000

    Chore: Replace firebase messaging sw env data

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -6,13 +6,11 @@ importScripts(
 // importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FB_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FB_MEASUREMENT_ID,
+  apiKey: "AIzaSyD21vD66EKhrhmn_tMSYDDGTFmSVgdyk1o",
+  projectId: "time-timer-2dfc2",
+  storageBucket: "time-timer-2dfc2.appspot.com",
+  messagingSenderId: "967984437557",
+  appId: "1:967984437557:web:d995fb425e91b470d4547c",
 });
 
 const messaging = firebase.messaging();
