dead828ec638ee796ea5f32285dcfdfe05f7a6d6
fecapark <fecapark@gmail.com>
2022-12-01 11:42:10 +0000

    Chroe: Change production basepath '/' -> ''

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const isDevServer = process.env.NODE_ENV === "development";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  basePath: isDevServer ? "/absproxy/3000" : "/",
+  basePath: isDevServer ? "/absproxy/3000" : "",
 };
 
 module.exports = withPWA(nextConfig);
