0a4182bb3cc80e881ad19cd5caf971519980a7ff
fecapark <fecapark@gmail.com>
2022-12-03 02:19:03 +0900

    Add: Directory /public on git

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,16 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js"
+);
+// importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js");
+// importScripts("https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD21vD66EKhrhmn_tMSYDDGTFmSVgdyk1o",
+  projectId: "time-timer-2dfc2",
+  storageBucket: "time-timer-2dfc2.appspot.com",
+  messagingSenderId: "967984437557",
+  appId: "1:967984437557:web:d995fb425e91b470d4547c",
+});
+
+const messaging = firebase.messaging();
