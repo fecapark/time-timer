47739bf54e144e346becd94201eb364abc96303f
fecapark <fecapark@gmail.com>
2022-12-04 09:25:49 +0000

    Feature: Loging background message payload

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -15,3 +15,17 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = "Background Message Title";
+  const notificationOptions = {
+    body: "Background Message body.",
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
