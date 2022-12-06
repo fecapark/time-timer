671831a6115143fe36a0d32a2ef15d1eae8059f6
fecapark <fecapark@gmail.com>
2022-12-06 14:39:01 +0000

    Chore: Change renamed modules

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,12 @@
 import styled from "@emotion/styled";
 import { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
-import { getMessagingToken } from "../backend/getMessagingToken";
 import { loadAudios } from "../backend/loadAudios";
 import Clock from "../components/Clock/Clock";
 import Footer from "../components/Layouts/Footer/Footer";
 import Header from "../components/Layouts/Header/Header";
 import Timer from "../components/Timer/Timer";
-import {
-  isNotificationPermissionGranted,
-  isNotificationSupportEnvironmentAtom,
-  soundEffectAudioAtom,
-} from "../shared/atom";
-
-import firebase from "firebase/app";
-import "firebase/messaging";
-import { firebaseConfig } from "../backend/firebaseConfig";
+import { soundEffectAudioAtom } from "../shared/atom";
 
 const Container = styled.div`
   width: 100%;
@@ -46,17 +37,6 @@ export default function Home() {
       },
     });
   }, []);
-
-  // useEffect(() => {
-  //   async function getToken() {
-  //     const messaging = firebase.messaging();
-  //     const token = await messaging.getToken({
-  //       vapidKey: process.env.NEXT_PUBLIC_FB_MESSAGING_KEY,
-  //     });
-  //   }
-
-  //   getToken();
-  // }, []);
 
   return (
     <Container>
