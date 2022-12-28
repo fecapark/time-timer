8151c378ac94e0aac405df86de28f2ea8bb39e53
fecapark <fecapark@gmail.com>
2022-12-28 04:56:13 +0000

    Feature: Add autoplay option on useAudio hook when getting permission

--- hooks/useAudio.ts
+++ hooks/useAudio.ts
@@ -1,11 +1,15 @@
 import { useCallback, useEffect, useState } from "react";
 
+interface IPermissionOption {
+  autoplayWhenAccepted?: boolean;
+}
+
 const dummyAudioSrc =
   "data:audio/mpeg;base64,SUQzBAAAAAABEVRYWFgAAAAtAAADY29tbWVudABCaWdTb3VuZEJhbmsuY29tIC8gTGFTb25vdGhlcXVlLm9yZwBURU5DAAAAHQAAA1N3aXRjaCBQbHVzIMKpIE5DSCBTb2Z0d2FyZQBUSVQyAAAABgAAAzIyMzUAVFNTRQAAAA8AAANMYXZmNTcuODMuMTAwAAAAAAAAAAAAAAD/80DEAAAAA0gAAAAATEFNRTMuMTAwVVVVVVVVVVVVVUxBTUUzLjEwMFVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQsRbAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVf/zQMSkAAADSAAAAABVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV";
 
 export default function useAudio(
   src: string | undefined | null
-): [() => void, () => void] {
+): [(option: IPermissionOption) => void, () => void] {
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
   const [isPlayable, setIsPlayable] = useState(false);
 
@@ -13,24 +17,31 @@ export default function useAudio(
     setAudio(new Audio());
   }, []);
 
-  const getPermission = useCallback(() => {
-    if (!audio) return;
-    if (isPlayable) return;
-    if (!src || src === "") return;
+  const getPermission = useCallback(
+    ({ autoplayWhenAccepted = false }: IPermissionOption) => {
+      if (!audio) return;
+      if (isPlayable) return;
+      if (!src || src === "") return;
 
-    audio.onloadeddata = () => {
-      audio.play();
-    };
+      audio.onloadeddata = () => {
+        audio.play();
+      };
 
-    audio.onended = () => {
-      audio.src = src;
-      audio.onloadeddata = null;
-      audio.onended = null;
-      setIsPlayable(true);
-    };
+      audio.onended = () => {
+        audio.src = src;
+        audio.onloadeddata = () => {
+          setIsPlayable(true);
+          audio.onloadeddata = null;
 
-    audio.src = dummyAudioSrc;
-  }, [audio, isPlayable, src]);
+          if (autoplayWhenAccepted) audio.play();
+        };
+        audio.onended = null;
+      };
+
+      audio.src = dummyAudioSrc;
+    },
+    [audio, isPlayable, src]
+  );
 
   const play = useCallback(() => {
     if (!audio) return;
