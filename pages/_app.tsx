62d782b6701440d1d674266f8b134777c5139b8e
fecapark <fecapark@gmail.com>
2023-01-11 12:05:42 +0000

    Feature: Add query client provider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,14 +4,19 @@ import { RecoilRoot } from "recoil";
 import GlobalTheme from "../styles/global";
 import { Theme } from "../styles/theme";
 import { Analytics } from "@vercel/analytics/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const qc = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <RecoilRoot>
         <ThemeProvider theme={Theme}>
-          <GlobalTheme />
-          <Component {...pageProps} />
+          <QueryClientProvider client={qc}>
+            <GlobalTheme />
+            <Component {...pageProps} />
+          </QueryClientProvider>
         </ThemeProvider>
       </RecoilRoot>
       <Analytics />
